81db16d226a949424e1f8f99f47e4aab09d77e04
ehan03 <eugeneh442@gmail.com>
2024-10-12 04:25:58 -0400

    initial report skeleton structure

---
+++ main.R
@@ -0,0 +1,26 @@
+#' ---
+#' title: "S&DS 625: Housing Project"
+#' date: "`r Sys.Date()`"
+#' author: "Matt Shu, Peter Yun, Eugene Han"
+#' output: pdf_document
+#' urlcolor: blue
+#' ---
+#' 
+#' # Introduction
+#' 
+#' # Methodology
+#' 
+#' # Data Preprocessing and Exploration
+#' ## Subset Construction
+#' 
+#' ## Exploration of Base Features
+#' 
+#' ## Food Access Features
+#' 
+#' # Modeling
+#' ## Baseline Model
+#' 
+#' ## Proposed Model
+#' 
+#' 
+#' # Conclusion
